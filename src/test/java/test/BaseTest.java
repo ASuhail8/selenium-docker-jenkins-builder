6d94a1e0fe45ba687945ce3c529fa69696a980b7
Abdulla Suhail <abdulla.suhail@gmail.com>
2021-10-16 20:37:27 +0530

    added a new testcase

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -1,14 +1,13 @@
 package test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class BaseTest {
 
