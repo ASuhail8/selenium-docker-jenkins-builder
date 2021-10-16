158dbebf4f03b7f9f31a7edb105e6c4fd2047625
Abdulla Suhail <abdulla.suhail@gmail.com>
2021-10-16 23:22:44 +0530

    CI/CD

--- src/test/java/com/searchmodule/tests/SearchTest.java
+++ src/test/java/com/searchmodule/tests/SearchTest.java
@@ -10,20 +10,14 @@ import test.BaseTest;
 
 public class SearchTest extends BaseTest {
 
-    @Test
-    @Parameters({"keyword"})
-    public void search(String keyword){
-        SearchPage searchPage = new SearchPage(driver);
-        searchPage.goTo();
-        searchPage.doSearch(keyword);
-        searchPage.goToVideos();
-        int result =  searchPage.getResult();
-        Assert.assertTrue(result>0);
-    }
-    
-    @Test
-    public void justGoToTheSite() {
-    	 SearchPage searchPage = new SearchPage(driver);
-         searchPage.goTo();	
-    }
+	@Test
+	@Parameters({ "keyword" })
+	public void search(String keyword) {
+		SearchPage searchPage = new SearchPage(driver);
+		searchPage.goTo();
+		searchPage.doSearch(keyword);
+		searchPage.goToVideos();
+		int result = searchPage.getResult();
+		Assert.assertTrue(result > 0);
+	}
 }
