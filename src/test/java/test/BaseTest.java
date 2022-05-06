4416aa04a656a7d945e2dbbc9761b47c6e53d7dd
Abdulla Suhail <abdullasuhail@Suhails-MacDoo.local>
2022-05-06 11:17:20 +0530

    changed basetest

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -3,10 +3,14 @@ package test;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -15,25 +19,28 @@ public class BaseTest {
 	protected WebDriver driver;
 
 	@BeforeTest
-	public void setupDriver() throws MalformedURLException {
+	public void setupDriver(ITestContext ctx) throws MalformedURLException {
 		// BROWSER => chrome/firefox
 		// HUB_HOST => localhost / 10.0.1.3 / hostname
 
 		String host = "localhost";
+		MutableCapabilities dc;
+
+		if (System.getProperty("BROWSER") != null && System.getProperty("BROWSER").equalsIgnoreCase("firefox")) {
+			dc = new FirefoxOptions();
+		} else {
+			dc = new ChromeOptions();
+		}
 
 		if (System.getProperty("HUB_HOST") != null) {
 			host = System.getProperty("HUB_HOST");
 		}
 
-		String completeUrl = "http://" + host + ":4444/wd/hub";
+		String testName = ctx.getCurrentXmlTest().getName();
 
-		if (System.getProperty("BROWSER") != null && System.getProperty("BROWSER").equalsIgnoreCase("firefox")) {
-			FirefoxOptions firfoxOptions = new FirefoxOptions();
-			driver = new RemoteWebDriver(new URL(completeUrl), firfoxOptions);
-		} else {
-			ChromeOptions chromeOptions = new ChromeOptions();
-			driver = new RemoteWebDriver(new URL(completeUrl), chromeOptions);
-		}
+		String completeUrl = "http://" + host + ":4444/wd/hub";
+		dc.setCapability("name", testName);
+		this.driver = new RemoteWebDriver(new URL(completeUrl), dc);
 
 	}
 
