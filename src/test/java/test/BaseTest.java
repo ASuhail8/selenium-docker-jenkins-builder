08dfd739480a1fb0f7e559649a6e33c5e176713c
Abdulla Suhail <abdullasuhail@Suhails-MacDoo.local>
2022-05-06 11:30:54 +0530

    updated compose file

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -4,11 +4,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.MutableCapabilities;
-import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterTest;
